ea8050838de737c0d48addb9392d3436b7ac067d
marianorapa <marianorapaport@gmail.com>
2024-03-22 19:08:13 -0300

    basic get endpoint

--- src/main/java/com/example/demo/controller/ProcessController.java
+++ src/main/java/com/example/demo/controller/ProcessController.java
@@ -12,11 +12,10 @@ public class ProcessController {
 
     @GetMapping("/process")
     public ResponseEntity<ProcessResponse> process() {
-        var restTemplate = new RestTemplate();
         System.out.println("Request received");
 
-        var response = restTemplate.getForEntity("http://127.0.0.1:6000/process", ProcessResponse.class);
-        return new ResponseEntity<>(response.getBody(), response.getStatusCode());
+        ProcessResponse response = new ProcessResponse(10);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
